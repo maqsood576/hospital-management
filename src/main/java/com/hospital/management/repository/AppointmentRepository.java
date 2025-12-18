6d372389a895e69728a9537cabad79b1be906bd1
maqsood576 <mehermaqsood112233@gmail.com>
2025-12-18 09:49:39 +0500

    Fix project structure for Railway Java build

--- src/main/java/com/hospital/management/repository/AppointmentRepository.java
+++ src/main/java/com/hospital/management/repository/AppointmentRepository.java
@@ -1,4 +1,4 @@
-package com.hospital.management.repositories;
+package com.hospital.management.repository;
 
 import com.hospital.management.models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
